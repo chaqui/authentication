f149bfe46303ce2b565da1bce4db88a6f1de196b
felepo <fecrackle@gmail.com>
2024-08-29 19:40:10 -0600

    fixing the roles path endpoints

--- services/roles.js
+++ services/roles.js
@@ -13,7 +13,6 @@ class RolesServices {
       await this.storage.addRoles(roleId, name, description);
       res.json({ roleId, name });
     } catch (error) {
-      console.log(error);
       res.status(500).json({ error: "Could not create role" });
     }
   }
@@ -29,25 +28,14 @@ class RolesServices {
     }
   }
 
-    async getRoles(res) {
-
-        try {
-            const roles = await this.storage.getRoles();
-            res.json(roles);
-        } catch (error) {
-            res.status(500).json({ error: "Could not return roles", message: error.message });
-        }
-    }
-
-    async getRole(roleId, res) {
-
-        try {
-            const rol = await this.storage.getRole(roleId);
-            res.json(rol);
-        } catch (error) {
-            console.log(error);
-            res.status(500).json({ error: "Could not return role", message: error.message });
-        }
+  async getRole(roleId, res) {
+    try {
+      const rol = await this.storage.getRole(roleId);
+      res.json(rol);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ error: "Could not return role", message: error.message });
     }
   }
 }
