4bc2291a1f05d3b25bff9ef0c2b381c260aadd4a
josue fuentes <josue.chaqui@gmail.com>
2024-09-11 16:18:03 -0600

    ✅ add tests for Users

--- test/users.test.js
+++ test/users.test.js
@@ -1,36 +1,50 @@
+const bcrypt = require("bcrypt");
 const UserServices = require("../services/users");
 
 class UserStore {
   async getUsers() {
-    return [];
+    return [
+      {
+        userId: "123",
+        name: "test",
+        userRoles: [],
+      },
+      {
+        userId: "456",
+        name: "test2",
+        userRoles: [],
+      },
+    ];
   }
-
-  async addUser() {
-    return {};
-  }
-
-  async getUserByName(name) {
-    return {};
-  }
-
-  async postUsers(user) {
+  async addUser(user) {
     return user;
   }
 }
 
-jest.mock("../store/users");
-
 describe("User Services", () => {
-  const store = new UserStore();
-  const userServices = new UserServices(store);
-  it("should call getUsers", () => {
-    let users = userServices.getUsers();
-    expect(userServices.getUsers).not.toBeNull();
+  let store = new UserStore();
+  test("should call getUsers", async () => {
+    const userServices = new UserServices(store);
+    const users = await userServices.getUsers();
+    console.log(users);
+    expect(users).not.toBeNull();
+    expect(users.length).toBe(2);
   });
 
-  it("should call postUsers", () => {
+  test("should call postUsers", async () => {
+    const userServices = new UserServices(store);
     const body = { name: "test", password: "test" };
-    userServices.postUsers(body);
-    expect(userServices.postUsers).not.toBeNull();
+    let user = await userServices.postUsers(body);
+    expect(user).not.toBeNull();
+    expect(user.name).toBe(body.name);
+  });
+
+  test("should convert password to hash", async () => {
+    const userServices = new UserServices(store);
+    const body = { name: "test", password: "test" };
+    let user = await userServices.postUsers(body);
+    expect(user).not.toBeNull();
+    expect(user.password).not.toBe(body.password);
+    expect(bcrypt.compare(body.password, user.password)).toBeTruthy();
   });
 });
